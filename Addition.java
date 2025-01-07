d725dcd2e76d902dd173876e9b1c5b9f4b82f2df
Aayush501 <shrivastavashu501@gmail.com>
2025-01-07 13:58:11 +0530

    added an addition program

---
+++ Addition.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Add{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int k = sc.nextInt();
+		int l = sc.nextInt();
+		System.out.println(k+l);
+	}
+}
